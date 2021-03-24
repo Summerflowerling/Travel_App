2ad5c84df7b78e40002e5c26e17d2b190183ca1f
yuling <yulinglin@protonmail.com>
2021-03-24 14:26:53 +0800

    Fix the sepelling of varialbe name destinationInput. Now the function works fine. Added Client library to index.html

--- src/client/js/getLocation.js
+++ src/client/js/getLocation.js
@@ -7,7 +7,7 @@ export function getLocation(){
 export function handleSubmit(event){
     event.preventDefault()
     let destinaitonInput = document.getElementById("destination").value
-    test.innerHTML=`<p>${destinationInput}</p>`
+    test.innerHTML=`<p>${destinaitonInput}</p>`
     console.log(destinaitonInput)
   
 
