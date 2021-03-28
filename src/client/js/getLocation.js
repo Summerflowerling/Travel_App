b51b0f7937ab90154a7eed9f1ad5f6b1d530ccea
yuling <yulinglin@protonmail.com>
2021-03-28 16:57:34 +0800

    Changed the geoname url to http://api.geonames.org/searchJSON? Finished the post request from the client side: can't not get the data and no error

--- src/client/js/getLocation.js
+++ src/client/js/getLocation.js
@@ -1,16 +1,44 @@
 const test = document.getElementById("test")
+const geonamesBaseUrl = "http://api.geonames.org/searchJSON?"
+let placeName=""
+let lat
+let lng
 
-export function getLocation(){
-    console.log("show me")
-}
 
-export function handleSubmit(event){
+
+/*fetch data from open weather api*/
+
+export  async function getLocation(input) {
+    
+    fetch('http://localhost:8080/getGeoname',{
+        method: 'POST',
+        credentials: 'same-origin',
+       mode: 'cors',
+        headers:{
+            'Content-Type':'application/json',
+        }, 
+        body:JSON.stringify({"location":`${input}`}),
+        
+    })
+    .then(res=> res.json())
+    .then(json=>{
+        console.log(json)
+    })
+       
+  }
+
+
+ 
+
+export  function handleSubmit(event){
     event.preventDefault()
-    let destinaitonInput = document.getElementById("destination").value
-    test.innerHTML=`<p>${destinaitonInput}</p>`
-    console.log(destinaitonInput)
-  
+    let destinationInput = document.getElementById("destination").value
+    test.innerHTML=`<p>${destinationInput}</p>`
+    console.log(destinationInput)
+    getLocation(destinationInput)
+    
+  }
+   
 
-}
 
 
--- src/server/server.js
+++ src/server/server.js
@@ -1,11 +1,16 @@
 let projectData = {}
+const geonamesBaseUrl = "http://api.geonames.org/searchJSON?"
 var path = require('path')
+const dotenv = require('dotenv');
+dotenv.config();
+const geonameApiKey = process.env.GEONAME_API ;
 const express = require('express')
 const app = express() 
 app.use(express.static('dist'))
 
 app.listen(8080, function(){
     console.log("Testing on port 8080")
+    console.log(`geo name api key ${geonameApiKey}`)
 })
 
 const bodyParser = require('body-parser')
@@ -17,6 +22,26 @@ const cors = require("cors")
 app.use(cors())
 
 app.get('/', function (req, res) {
-    //res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/view/index.html'))
+    res.sendFile('dist/index.html')
+    //res.sendFile(path.resolve('src/client/view/index.html'))
 })
+
+
+
+//q=undefined&maxRows=1&username=undefined
+app.post("/getGeoname", async function(req,res){
+    const myPromise = await fetch(geonamesBaseUrl+`q=${req.body.location}&maxRows=1&username=${geonameApiKey}`); 
+    
+    try{
+        const myData = await myPromise.json();  
+        res.send(myData)
+       
+    }
+       
+    catch(error){
+        alert("Make sure you enter a country or city name")
+        return;
+    }
+
+})
+
