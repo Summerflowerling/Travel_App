0032b9408a5e6dddb31fd8956a3c6af7ce8bb95e
yuling <yulinglin@protonmail.com>
2021-04-02 08:47:53 +0800

    added travelfrom  input value

--- src/client/js/getDays.js
+++ src/client/js/getDays.js
@@ -1,14 +1,26 @@
-let travelDate
-let presentDate = new Date()
+
 // One day Time in ms (milliseconds)
 let oneDayMs = 1000 * 60 * 60 * 24
+let inputStartDate = document.getElementById("travelStartDate").value
+let inputEndDate = document.getElementById("travelEndDate").value
 
-export function getDays (travelMonth, travelDay, travelYear){
-    travelDate = new Date(`${travelMonth}/${travelDay}/${travelYear}`)
+
+
+// Calculate the trip duration
+export function getDays (inputStartDate, inputEndDate){
+    
+
+    let travelStart = new Date (inputStartDate)
+    let travelEnd = new Date (inputEndDate)
+
+    console.log("travelStartDate", travelStart)
+    console.log("travelEndDate", travelEnd )
     // To calculate the time difference of two dates
-    let differenceInTime = travelDate.getTime() - presentDate.getTime();
+    let differenceInTime = travelEnd.getTime() - travelStart.getTime();
   
     // To calculate the no. of days between two dates
     let differenceInDays = differenceInTime / (oneDayMs);
     console.log(Math.round(differenceInDays))
+    let duration = Math.round(differenceInDays)
+    return duration 
 }
