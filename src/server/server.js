e3d24261b1aed0a9ca2c931e79608f5ca2b879ff
yuling <yulinglin@protonmail.com>
2021-04-13 20:10:26 +0800

    Deleted default value in #travelEndDate and #travelStartDate index.html

--- src/server/server.js
+++ src/server/server.js
@@ -3,6 +3,7 @@ const geonamesBaseUrl = "http://api.geonames.org/searchJSON?"
 const weatherBitUrl = "https://api.weatherbit.io/v2.0/forecast/daily?"
 const pixabayUrl="https://pixabay.com/api/?key=4772361-58a041a9c4a31b16cbe90fbc1&q=yellow+flowers&image_type=photo"
 var path = require('path')
+
 /*.env and api keys*/
 const dotenv = require('dotenv');
 dotenv.config();
@@ -40,10 +41,10 @@ app.get('/', function (req, res) {
 
 
 
-
+/*NOTE: replace the api key with stored variable will crush, need help to debug */
 app.post("/getGeoname", async function(req,res){
    
-    const API_URL = `http://api.geonames.org/searchJSON?q=${req.body.location}&maxRows=1&username=${geonameApiKey}` 
+    const API_URL = `http://api.geonames.org/searchJSON?q=${req.body.location}&maxRows=1&username=iku124` 
 
     const myPromise = await fetch(API_URL); 
     
@@ -52,12 +53,12 @@ app.post("/getGeoname", async function(req,res){
         let lng = myData.geonames[0].lng
         let lat = myData.geonames[0].lat
         
-        const weatherbit = await fetch (weatherBitUrl+`lat=${lat}&lon=${lng}&days=7&key=${weatherbitKey}`)
+        const weatherbit = await fetch (weatherBitUrl+`lat=${lat}&lon=${lng}&days=7&key=25236c54b21347c5acba0d34020a5c84`)
        
         try{
             const weatherbitPromise = await weatherbit.json()
             console.log(weatherbitPromise)
-            const pixabay = await fetch(`https://pixabay.com/api/?key=${pixabayKey}&q=${req.body.location}&image_type=photo&editors_choice=true&category=travel`)
+            const pixabay = await fetch(`https://pixabay.com/api/?key=4772361-58a041a9c4a31b16cbe90fbc1&q=${req.body.location}&image_type=photo&editors_choice=true&category=travel`)
 
             try{
                 const pixabayPrimose = await pixabay.json()
