4e31968862368eb5ec2823427e302ba7ee1a74fd
yuling <yulinglin@protonmail.com>
2021-05-30 22:17:48 +0800

    refactor: Add url loader in both dev and prod.js
    local imag can't display in prod mode.
    Add url loader to make sure img can be loaded in prod as well.

--- webpack.dev.js
+++ webpack.dev.js
@@ -23,6 +23,13 @@ module.exports = {
             {
                 test: /\.scss$/,
                 use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+            },
+            {
+                test: /\.(jpg|png)$/,
+                use: [
+                     { loader: 'url-loader'},
+                ]
+           
             }
         ]
     },
